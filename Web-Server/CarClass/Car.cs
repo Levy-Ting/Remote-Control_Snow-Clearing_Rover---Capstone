3e3047a414feea51d7ccc01ef1d4106b1bcfb22c
tlevytskyy <60587034+tlevytskyy@users.noreply.github.com>
2021-12-19 18:16:29 -0700

    Added Server and Camera Code

---
+++ Web-Server/CarClass/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    public class Car
+    {
+        public Car()
+        {
+            CarID = 1;
+            Directions = 'S';
+        }
+
+        /// <summary>
+        /// CarID will be used to hold an int that represents the ID of the Car connecting to the WebAPI
+        /// </summary>
+        public int CarID { get; set; }
+
+        /// <summary>
+        /// Directions will be used to hold a char that represents the Direction the car is supposed to move in
+        /// </summary>
+        public char Directions { get; set; }
+    }
+}
---
+++ Web-Server/SnowAPI/Controllers/CarController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarClass;
+
+namespace SnowAPI.Controllers
+{
+    [Route("api/Car")]
+    [ApiController]
+    public class CarController : Controller
+    {
+        static Car thisCar = new Car();
+
+        /// <summary>
+        /// GET Method will allow the website to retrieve the Car ID and Direction for the Car 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetCarInfo")]
+        public Car GetCarInfo()
+        {
+            return thisCar;
+        }
+
+        /// <summary>
+        /// POST Method will allow the API to retrieve the user input for the car ***Future use for carID
+        /// </summary>
+        /// <param name="carID"></param>
+        /// <param name="Directions"></param>
+        [HttpGet]
+        [Route("SendCarInfo")]
+        public ActionResult<char> SendCarInfo(char Directions)
+        {
+            thisCar.Directions = Directions;
+            return Ok(thisCar.Directions);
+
+        }
+
+        /// <summary>
+        /// GET Method will allow the car to retrieve the direction 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetCommand")]
+        public char GetCommand()
+        {
+            return thisCar.Directions;
+        }
+    }
+}
